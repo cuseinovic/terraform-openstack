e9c06cef5ee56ce454db44205ba2a11fede00a23
Christophe Useinovic <christophe.useinovic@objectif-libre.com>
2020-01-20 14:38:21 +0100

    update port information

--- sg.tf
+++ sg.tf
@@ -1,5 +1,6 @@
 resource "openstack_compute_secgroup_v2" "http" {
   name        = "http"
+  description  = "http port open IN/OUT"
   rule {
     from_port   = 80
     to_port     = 80
@@ -10,6 +11,7 @@ resource "openstack_compute_secgroup_v2" "http" {
 
 resource "openstack_compute_secgroup_v2" "ssh" {
   name        = "ssh"
+  description = "ssh port open IN/OUT"
   rule {
     from_port   = 22
     to_port     = 22
